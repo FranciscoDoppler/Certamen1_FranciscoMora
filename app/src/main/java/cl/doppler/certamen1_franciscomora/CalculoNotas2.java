c7927d277a74dc6227a4acb0c41adcef82fd8bab
Francisco Doppler <francisco.mora@virginiogomez.cl>
2021-10-17 13:54:15 -0300

    CertamenV01

---
+++ app/src/main/java/cl/doppler/certamen1_franciscomora/CalculoNotas2.java
@@ -0,0 +1,48 @@
+package cl.doppler.certamen1_franciscomora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalculoNotas2 extends AppCompatActivity {
+
+    private Button botoNN;
+    private String Nombre;
+    private TextView tvNombre;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo_notas2);
+
+        tvNombre= (TextView) findViewById(R.id.TVNombre);
+
+        botoNN= (Button) findViewById(R.id.BotonBACK);
+        Intent intent2= new Intent(this,MainActivity.class);
+
+        Context context=getApplicationContext();
+
+        Bundle intentN=this.getIntent().getExtras();
+        if (intentN!=null){
+            Nombre=intentN.getString("nombre");
+            Toast.makeText(context,"Estudiante: " +Nombre,Toast.LENGTH_SHORT).show();
+
+        }
+
+        tvNombre.setText("Estudiante : " +System.lineSeparator()+System.lineSeparator() +Nombre);
+
+        botoNN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent2);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/cl/doppler/certamen1_franciscomora/MainActivity.java
@@ -0,0 +1,42 @@
+package cl.doppler.certamen1_franciscomora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botoN;
+    private EditText etNombre;
+    private EditText nota1, nota2, nota3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nota1=(EditText) findViewById(R.id.ETNota1);
+        nota2=(EditText) findViewById(R.id.ETNota2);
+        nota3=(EditText) findViewById(R.id.ETNota3);
+
+        etNombre=(EditText) findViewById(R.id.ETNombre);
+
+        botoN =(Button) findViewById(R.id.BotonSIG);
+        Intent intent1=new Intent(this,CalculoNotas2.class);
+
+        botoN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                intent1.putExtra("nombre", etNombre.getText().toString());
+
+                startActivity(intent1);
+            }
+        });
+    }
+}
